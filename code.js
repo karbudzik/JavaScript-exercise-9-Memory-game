606429b820cb8204abaa95f9a55c7739f0d0e78a
Karolina <karolinabudzik92@gmail.com>
2020-05-18 17:24:32 +0200

    Add function makeMove()

--- code.js
+++ code.js
@@ -1,7 +1,8 @@
 var cards = document.querySelectorAll(".card");
 cards = Array.from(cards);
+var cardsFromThisTurn = new Array();
 
-function addClassesToCards() {
+function shuffleCards() {
     var numbers = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
     for (var card of cards) {
         numbers = Array.from(numbers);
@@ -17,14 +18,33 @@ function addClassesToCards() {
 function handleClick() {
     for (var i = 0; i < cards.length; i++){
         cards[i].addEventListener("click", displayCard);
+        cards[i].addEventListener("click", makeMove);
     };
 }
 
-var displayCard = function (){
+function displayCard() {
     this.classList.toggle("closed");
     this.classList.toggle("disabled");
-    console.log("ggg");
 }
 
-addClassesToCards();
-handleClick();
+function makeMove() {
+    console.log("start");
+    cardsFromThisTurn.push(this);
+    console.log(cardsFromThisTurn);
+    if (cardsFromThisTurn.length === 2) {
+        console.log("Są dwie karty");
+        if (getComputedStyle(cardsFromThisTurn[0]).backgroundImage === getComputedStyle(cardsFromThisTurn[1]).backgroundImage) {
+            handleWonTurn()
+        } else {
+            handleLostTurn()
+        }
+    }
+}
+
+
+function startGame() {
+    shuffleCards();
+    handleClick();
+}
+
+window.addEventListener("load", startGame());
