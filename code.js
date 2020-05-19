ac16bbf20bcc8c864ec0427b5c5d97a745cb6242
Karolina <karolinabudzik92@gmail.com>
2020-05-19 23:19:33 +0200

    Finish basic game logic

--- code.js
+++ code.js
@@ -1,6 +1,12 @@
-var cards = document.querySelectorAll(".card");
-cards = Array.from(cards);
-var cardsFromThisTurn = new Array();
+class Player  {
+    constructor(name, points) {
+      this.name = name;
+      this.points = points;
+    }
+}
+
+const cards = Array.from(document.querySelectorAll(".card"));
+let cardsFromThisTurn = new Array();
 
 function shuffleCards() {
     var numbers = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
@@ -15,6 +21,68 @@ function shuffleCards() {
     }
 }
 
+function displayCard() {
+    this.classList.toggle("closed");
+    this.classList.toggle("disabled");
+}
+
+function makeMove() {
+    cardsFromThisTurn.push(this);
+    if (cardsFromThisTurn.length === 2) {
+        if (getComputedStyle(cardsFromThisTurn[0]).backgroundImage === getComputedStyle(cardsFromThisTurn[1]).backgroundImage) {
+            
+            console.log("It was won turn of " + window.currentPlayer.name + "with points " + window.currentPlayer.points + " +2");
+            handleWonTurn()
+            console.log(window.cardsToGuess + " cards to guess left");
+        } else {
+            
+            console.log("It was lost turn of " + window.currentPlayer.name + "with points " + window.currentPlayer.points);
+            handleLostTurn()
+            console.log(window.cardsToGuess + " cards to guess left");
+        }
+    }
+}
+
+function handleWonTurn() {
+    cardsFromThisTurn[0].classList.add("used");
+    cardsFromThisTurn[1].classList.add("used");
+
+    window.currentPlayer.points += cardsFromThisTurn.length;
+    window.cardsToGuess -= cardsFromThisTurn.length;
+    cardsFromThisTurn.length = 0;
+    checkIfGameEnded();
+}
+
+function handleLostTurn() {
+    cardsFromThisTurn[0].classList.remove("disabled");
+    cardsFromThisTurn[1].classList.remove("disabled");
+    cardsFromThisTurn[0].classList.add("closed");
+    cardsFromThisTurn[1].classList.add("closed");
+
+    cardsFromThisTurn.length = 0;
+    changePlayer();
+}
+
+function changePlayer() {
+    window.currentPlayer = (window.currentPlayer === player1) ? window.player2 : window.player1;
+}
+
+function checkIfGameEnded() {
+    if (window.cardsToGuess <= 0) {
+        announceWinner();
+    }
+}
+
+function announceWinner() {
+    if (window.player1.points > window.player2.points) {
+        console.log("Winner is " + window.player1.name)
+    } else if (window.player2.points > window.player1.points) {
+        console.log("Winner is " + window.player2.name)
+    } else {
+        console.log("It's a tie!");
+    }
+}
+
 function handleClick() {
     for (var i = 0; i < cards.length; i++){
         cards[i].addEventListener("click", displayCard);
@@ -22,27 +90,11 @@ function handleClick() {
     };
 }
 
-function displayCard() {
-    this.classList.toggle("closed");
-    this.classList.toggle("disabled");
-}
-
-function makeMove() {
-    console.log("start");
-    cardsFromThisTurn.push(this);
-    console.log(cardsFromThisTurn);
-    if (cardsFromThisTurn.length === 2) {
-        console.log("Są dwie karty");
-        if (getComputedStyle(cardsFromThisTurn[0]).backgroundImage === getComputedStyle(cardsFromThisTurn[1]).backgroundImage) {
-            handleWonTurn()
-        } else {
-            handleLostTurn()
-        }
-    }
-}
-
-
 function startGame() {
+    window.player1 = new Player("player1", 0);
+    window.player2 = new Player("player2", 0);
+    window.currentPlayer = player1;
+    window.cardsToGuess = cards.length;
     shuffleCards();
     handleClick();
 }
